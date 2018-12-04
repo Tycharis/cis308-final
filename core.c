aa4604b84b4341be41b44ed7d1467398da3bb038
Braedon Smith <bhsmith1999@gmail.com>
2018-12-04 09:32:37 -0600

    Added start code
    Signed-off-by: Braedon Smith <bhsmith1999@gmail.com>

--- core.c
+++ core.c
@@ -4,9 +4,26 @@
  */
 
 #include <math.h>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    return 0;
+    if (argc == 1)
+    {
+        //TODO GUI
+    }
+    else if (argv[1] == "<")
+    {
+        //TODO scan file in argv[2]
+    }
+    else if (argv[1] != NULL)
+    {
+        //TODO scan string in argv[1]
+    }
+    else
+    {
+        print_usage();
+        return 0;
+    }
 }
 
 int evaluate(SyntaxTree * tree)
@@ -39,4 +56,11 @@ int evaluate(SyntaxTree * tree)
     }
 
     return NULL;
+}
+
+void print_usage()
+{
+    printf("Load GUI:        ./main");
+    printf("Evaluate string: ./main <input>");
+    printf("Load from file:  ./main < <file>")
 }
